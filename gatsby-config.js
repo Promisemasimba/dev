1d8e633b1e2534228c695dacc966cb179f57b8d6
Promisemasimba <57047533+Promisemasimba@users.noreply.github.com>
2021-01-22 22:22:04 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
